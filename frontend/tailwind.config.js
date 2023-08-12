7e993d4e9973e151d4fefcc2cfca1fe1f40e222a
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-08-13 00:22:07 +0200

    Add screens brakpoints

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "426px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     fontFamily: {
       poppins: ["Poppins"],
     },
