7fe250fcaab2b1ecfbae9a55cebd644493330bfa
Dudzinski-Robert <130515506+DudzinskiR@users.noreply.github.com>
2023-08-14 04:17:20 +0200

    Add title to box component

--- frontend/src/components/box/box.tsx
+++ frontend/src/components/box/box.tsx
@@ -9,6 +9,12 @@ interface props {
 const Box = ({ children, className, title }: props) => {
   return (
     <div className={`${className} bg-white rounded border-2 shadow`}>
+      {title && (
+        <div className="w-full pt-5 px-5">
+          <div className="text-xl mb-1 font-semibold">{title}</div>
+          <div className="w-full border mb-3"></div>
+        </div>
+      )}
       {children}
     </div>
   );
